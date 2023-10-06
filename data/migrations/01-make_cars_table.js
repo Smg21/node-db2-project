34b0e4ee6170f3df86212e5738723660e71b8b00
Sabrina <Smg21022@gmail.com>
2023-10-05 17:28:33 -0700

    passing

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,18 +1,21 @@
-exports.up = function(knex) {
-  return knex.schema.createTable("cars", tbl => {
-    tbl.increments();
-    tbl
-      .text("vin", 128)
-      .unique()
-      .notNullable();
-    tbl.text("make").notNullable();
-    tbl.text("model").notNullable();
-    tbl.integer("mileage").notNullable();
-    tbl.text("transmissionType");
-    tbl.text("titleStatus");
-  });
+exports.up = function (knex) {
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments()
+
+    tbl.string('vin', 17).notNullable().unique()
+
+    tbl.string('make', 128).notNullable()
+
+    tbl.string('model', 128).notNullable()
+    
+    tbl.integer('mileage').unsigned().notNullable()
+
+    tbl.string('title', 128)
+
+    tbl.string('transmission', 128)
+  })
 };
 
-exports.down = function(knex) {
-  return knex.schema.dropTableIfExists("cars");
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('cars')
 };
